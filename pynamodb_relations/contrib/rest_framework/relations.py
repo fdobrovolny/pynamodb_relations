389ad4770841b485b34328be23ceaa0012c270ee
Filip Dobrovolny <brnopcman@gmail.com>
2020-07-18 02:28:05 +0200

    Optimize DRF don't fetch related fields object when only ID is needed

--- pynamodb_relations/contrib/rest_framework/relations.py
+++ pynamodb_relations/contrib/rest_framework/relations.py
@@ -1,12 +1,29 @@
 from pynamodb.attributes import Attribute
+from rest_framework.fields import get_attribute
 from rest_framework.relations import RelatedField
+
+from pynamodb_relations.models import Model
 
 
 class UnicodeRelatedField(RelatedField):
+    """
+    This field serves as Proxy between Unicode Related field and DRF.
+
+    Attributes:
+        disable_related_object_resolve: If False object is obtained
+            from the DynamoDB. If True only it's id is used without
+            checking if the object does still exist.
+
+    """
     model_field: Attribute
+    disable_related_object_resolve: bool = True
 
     def __init__(self, **kwargs):
         self.model_field = kwargs.pop("model_field")
+        self.disable_related_object_resolve = kwargs.pop(
+            "disable_related_object_resolve",
+            self.disable_related_object_resolve
+        )
         super(UnicodeRelatedField, self).__init__(**kwargs)
 
     def to_representation(self, value):
@@ -14,3 +31,11 @@ class UnicodeRelatedField(RelatedField):
 
     def to_internal_value(self, data):
         return self.model_field.deserialize(data).get()
+
+    def get_attribute(self, instance: Model):
+        if self.disable_related_object_resolve:
+            attribute_instance: Model = get_attribute(instance, self.source_attrs[:-1])
+            if attribute_instance.attribute_values[self.source_attrs[-1]]._resolved is False:
+                return instance.attribute_values[self.source_attrs[-1]].value
+
+        return super().get_attribute(instance)
